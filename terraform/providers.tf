7a1ca76bb71717baf58a3b4203ca1a9a969aa579
NickMarini <nicholas.marini13@gmail.com>
2026-06-07 22:28:31 +0200

    Website infra and deployment dev and prod

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
+    google-beta = {
+      source  = "hashicorp/google-beta"
       version = "~> 5.0"
     }
   }
 }
 
-provider "google" {
+provider "google-beta" {
   project = var.project_id
   region  = var.region
 }
